4d6921f1f36aa0ae3aac479a83f20ec02704d265
sudhanpoudel2@gmail.com <sudhanpoudel2@gmail.com>
2023-11-19 13:29:23 +0545

    connect with DB

--- src/index.js
+++ src/index.js
@@ -1,14 +1,43 @@
-import mongoose  from "mongoose";
-import { DB_NAME } from "./constants";
+// require('dotenv').config({path : './env'});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
-/*function connectDB(){};
-connectDB()*/ 
+dotenv.config({
+    path: "./env"
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+/*
+import express from "excpress";
+
+const app = express();
+
+// function connectDB(){};
+// connectDB()
 // for write code porfectional we use another function
 
 ( async ()=>{
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("Error:", error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT, ()=>{
+         console.log(`App is listining on port ${process.env.PORT}`);
+        })
     } catch (error) {
         console.error("Error:", error)
     }
 })();
+
+*/
