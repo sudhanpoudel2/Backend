99d6dead36ba5f727aa3edb32166fe0118be2701
sudhanpoudel2@gmail.com <sudhanpoudel2@gmail.com>
2023-11-20 13:16:35 +0545

    api response

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+    app.on("error", (error) => {
+        console.log("Error:", error);
+        throw error;
+    })
+})
+.catch((error)=>{
+    console.log("MONGO db connection failed", error);
+})
 
 
 
