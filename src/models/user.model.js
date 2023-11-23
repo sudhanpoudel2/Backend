0acf4af764646287d09a5e86f9f976586de37bd4
sudhanpoudel2@gmail.com <sudhanpoudel2@gmail.com>
2023-11-23 08:17:28 +0545

    user model updatre

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,52 @@
 import mongoose ,{Schema} from "mongoose";
 
-const userSchema = new Schema({});
+const userSchema = new Schema(
+    {
+       username : {
+        type : String,
+        required : true,
+        unique : true,
+        lowercase : true,
+        trim : true,
+        index : true
+       },
+       email : {
+        type : String,
+        required : true,
+        unique : true,
+        lowercase : true,
+        trim : true,
+       },
+       fulname : {
+        type : String,
+        required : true,
+        trim : true,
+        index : true
+       },
+       avatar : {
+        type : String, //cloudinary url
+        required : true
+       },
+       coverimage : {
+        type : String, //cloudinary url
+       },
+       watchHistory : [
+        {
+            type : Schema.Type.ObjectId,
+            ref :"Video"
+        }
+       ],
+       password : {
+        type : String,
+        required : [true, 'password is required']
+       },
+       refereshToken : {
+        type : String
+       }
+    }
+    {
+        timestamps: true
+    }
+);
 
 export const User = model.Schema("User",userSchema);
