cb27e7ad03d018f26b2685210df0822d6fbd8db2
sudhanpoudel2@gmail.com <sudhanpoudel2@gmail.com>
2023-12-05 08:28:27 +0545

    rout

--- src/routes/user.routers.js
+++ src/routes/user.routers.js
@@ -4,5 +4,6 @@ import { registerUser } from "../controllers/user.controller";
 const router = Router();
 
 router.route("/register").post(registerUser)   //localhost:8000/user/register
+router.route("/login").post(login)             //localhost:8000/user/login
 
 export default router;
