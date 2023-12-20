91f154a1db61541b268a64756f38205c5c645bfd
sudhanpoudel2@gmail.com <sudhanpoudel2@gmail.com>
2023-12-20 22:08:35 +0545

    commited

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,9 +3,18 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 
 const registerUser = asyncHandler(async(req,res)=>{
-    res.status(200).json({
-        message : "OK"
-    })
+    // get user details from frontend
+    // validation - not empty
+    // check if user already exists : username , email
+    // check for image, check for avatar
+    // upload them to cloudinary, avatar
+    // create user object-create entry in db
+    // remove password and refresh token field from response 
+    // check from user creation 
+    // return res
+
+    const {fullname ,email ,username , password}= req.body
+    console.log("email",email);
    
 })
 
