da81ca0d8692c1708e25f1ee624da8f6efa82024
sudhanpoudel2@gmail.com <sudhanpoudel2@gmail.com>
2023-12-21 21:09:31 +0545

    commited

--- src/routes/user.routers.js
+++ src/routes/user.routers.js
@@ -1,9 +1,22 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js"
 
 const router = Router()
 
-router.route("/register").post(registerUser)   //localhost:8000/user/register
+router.route("/register").post(
+    upload.fields([
+        {
+            name : "avatar",
+            maxCount : 1
+        },
+        {
+            name : "coverImage",
+            maxCount : 1
+        }
+    ]),
+    registerUser
+    )   //localhost:8000/user/register
 // router.route("/login").post(login)             //localhost:8000/user/login
 
 export default router
