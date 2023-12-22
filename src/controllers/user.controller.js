5f9cf83a6fe05a7bb51618361755a2c605ed19d5
sudhanpoudel2@gmail.com <sudhanpoudel2@gmail.com>
2023-12-22 13:44:18 +0545

    login building

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js"
+import {uploadOnCloudinary} from '../utils/cloudinary.js'
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 
 const registerUser = asyncHandler(async(req,res)=>{
@@ -40,6 +42,35 @@ const registerUser = asyncHandler(async(req,res)=>{
     if (avatarLocalPath) {
         throw new ApiError(400, " avater file is required")
     }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if (!avatar) {
+        throw new ApiError(400, " avater file is required")
+    }
+
+
+    const user = await User.create({
+        fullname,
+        avatar : avatar.url,
+        coverImage : coverImage?.url || "",
+        email,
+        password,
+        username : username.toLowerCase()
+    })
+
+    const createdUser = User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if (!createdUser) {
+        throw new ApiError(400, "somthing went wrong while regestring the user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser , "User register successfully")
+    )
 })
 
 export {registerUser}
